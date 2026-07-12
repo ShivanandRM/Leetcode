5ed78a3fd34f1e97730f7f29a173ee37b950f33f
ShivanandM <shivanandmugali882@gmail.com>
2026-07-12 23:02:07 +0530

    Time: 0 ms (100%), Space: 44.6 MB (82.97%) - LeetHub

--- 0035-search-insert-position/0035-search-insert-position.java
+++ 0035-search-insert-position/0035-search-insert-position.java
@@ -5,9 +5,9 @@ class Solution {
         while(start <= end){
             int mid = start + (end - start) / 2;
             if(target < nums[mid]){
-                end = end - 1;
+                end = mid - 1;
             }else if(target > nums[mid]){
-                start = start +1;
+                start = mid +1;
             }else{
                 return mid;
             }
