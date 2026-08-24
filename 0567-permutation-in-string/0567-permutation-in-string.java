f9158c31fe32628cd602dfd3ae1ba26a3b7ea9c7
ShivanandRM <shivanandmugali882@gmail.com>
2026-08-24 23:00:36 +0530

    Time: 5 ms (97.48%), Space: 44 MB (73.71%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.java
+++ 0567-permutation-in-string/0567-permutation-in-string.java
@@ -1,41 +1,72 @@
 class Solution {
     public boolean checkInclusion(String s1, String s2) {
-        if (s1.length() > s2.length()) {
+         if (s1.length() > s2.length()) {
             return false;
         }
 
-        // Frequency of characters in s1
         int[] need = new int[26];
-
-        // Frequency of characters in current window
         int[] window = new int[26];
 
-        // Build frequency of s1
         for (char ch : s1.toCharArray()) {
             need[ch - 'a']++;
         }
 
         int left = 0;
+        int matches = 0;
 
         for (int right = 0; right < s2.length(); right++) {
 
-            // Character enters the window
-            window[s2.charAt(right) - 'a']++;
+            int index = s2.charAt(right) - 'a';
 
-            // Keep window size equal to s1.length()
+            window[index]++;
+
+            // Only track characters that s1 actually needs
+            if (need[index] > 0) {
+
+                if (window[index] == need[index]) {
+                    matches++;
+                }
+
+                else if (window[index] == need[index] + 1) {
+                    matches--;
+                }
+            }
+
+            // Keep fixed window size
             if (right - left + 1 > s1.length()) {
 
-                // Character leaves the window
-                window[s2.charAt(left) - 'a']--;
+                int leftIndex = s2.charAt(left) - 'a';
+
+                if (need[leftIndex] > 0) {
+
+                    if (window[leftIndex] == need[leftIndex]) {
+                        matches--;
+                    }
+
+                    else if (window[leftIndex] == need[leftIndex] + 1) {
+                        matches++;
+                    }
+                }
+
+                window[leftIndex]--;
 
                 left++;
             }
 
-            // Check whether current window is a permutation of s1
-            if (Arrays.equals(need, window)) {
+            // Number of distinct required characters in s1
+            int required = 0;
+
+            for (int i = 0; i < 26; i++) {
+                if (need[i] > 0) {
+                    required++;
+                }
+            }
+
+            if (matches == required) {
                 return true;
             }
         }
+
         return false;
     }
 }
