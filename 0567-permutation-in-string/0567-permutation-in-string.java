c65f0be0765a441302d3a29a22a807c9935d527f
ShivanandRM <shivanandmugali882@gmail.com>
2026-08-24 23:06:02 +0530

    Time: 7 ms (68.72%), Space: 43.8 MB (85.34%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.java
+++ 0567-permutation-in-string/0567-permutation-in-string.java
@@ -1,6 +1,8 @@
 class Solution {
     public boolean checkInclusion(String s1, String s2) {
-         if (s1.length() > s2.length()) {
+
+
+        if (s1.length() > s2.length()) {
             return false;
         }
 
@@ -12,57 +14,19 @@ class Solution {
         }
 
         int left = 0;
-        int matches = 0;
 
         for (int right = 0; right < s2.length(); right++) {
 
-            int index = s2.charAt(right) - 'a';
+            window[s2.charAt(right) - 'a']++;
 
-            window[index]++;
-
-            // Only track characters that s1 actually needs
-            if (need[index] > 0) {
-
-                if (window[index] == need[index]) {
-                    matches++;
-                }
-
-                else if (window[index] == need[index] + 1) {
-                    matches--;
-                }
-            }
-
-            // Keep fixed window size
             if (right - left + 1 > s1.length()) {
 
-                int leftIndex = s2.charAt(left) - 'a';
-
-                if (need[leftIndex] > 0) {
-
-                    if (window[leftIndex] == need[leftIndex]) {
-                        matches--;
-                    }
-
-                    else if (window[leftIndex] == need[leftIndex] + 1) {
-                        matches++;
-                    }
-                }
-
-                window[leftIndex]--;
+                window[s2.charAt(left) - 'a']--;
 
                 left++;
             }
 
-            // Number of distinct required characters in s1
-            int required = 0;
-
-            for (int i = 0; i < 26; i++) {
-                if (need[i] > 0) {
-                    required++;
-                }
-            }
-
-            if (matches == required) {
+            if (Arrays.equals(need, window)) {
                 return true;
             }
         }
@@ -70,3 +34,4 @@ class Solution {
         return false;
     }
 }
+   
